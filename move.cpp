71b28896117caec06e2101f946c45ef9ae6866d5
Thomas Dougherty <105173634+sauce-picante@users.noreply.github.com>
2023-05-30 09:27:42 -0400

    print

--- move.cpp
+++ move.cpp
@@ -19,8 +19,8 @@ Move::Move(int from, int to, Piece promotion) {
 Move::Move(std::string alg_notation) {
     std::string from_square = alg_notation.substr(0,2);
     std::string to_square = alg_notation.substr(2,2);
-    std::cout << "FROM: " << from_square << std::endl;
-    std::cout << "TO:   " << to_square << std::endl;
+    std::cout << "FROM: " << from_square << "\n";
+    std::cout << "TO:   " << to_square << "\n";
     from_index = Notation::toSquare(from_square);
     to_index = Notation::toSquare(to_square); // 'toSquare()' as in from a string, to a board square
     Promotion = alg_notation.length() == 5 ? Notation::toPiece(alg_notation[4]) : Piece::None;
