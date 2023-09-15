61130f1af1015ece445511634ce9d6c2f9b0b1fe
Thomas Dougherty <tdougherty16@gmail.com>
2023-09-15 11:01:41 -0400

    refactor

--- src/board-viz.cpp
+++ src/board-viz.cpp
@@ -10,36 +10,6 @@
 #define BLUE "\033[34m"
 #define YELLOW "\033[33m"
 
-void printBoard(Board& board);
-void printPiece(Piece piece);
-void playMove(Board& board, std::string moveNot);
-
-int main() {
-    // TO-DO: Make START_POS_FEN an argument from a file or command line entry
-    Board board1(Board::START_POS_FEN);
-    
-    printBoard(board1);
-    
-    std::string line;
-    while (std::getline(std::cin, line)) {
-        std::cout << "\n>> Move <<\n";
-        std::string input;
-        std::cin >> input;
-        std::vector<std::string> moves = board1.splitString(input, ' ');
-        
-            for(std::string move : moves) {
-                playMove(board1, move);
-                printBoard(board1);
-                std::cout<< "\n>>> Last Move: " << move << "\n";
-            }
-        
-        if (std::cin.eof()) {
-            std::cout << "End of game log..." << std::endl;
-            break;
-        }
-    }
-    return 0;
-}
 /** Updates the board state to the most recent move **/
  void playMove(Board& board, std::string moveNot) {
     Move move  = Notation::toMove(moveNot);
@@ -80,4 +50,29 @@ void printPiece(Piece piece){
     }   
 }
 
-
+int main() {
+    // TO-DO: Make START_POS_FEN an argument from a file or command line entry
+    Board board1(Board::START_POS_FEN);
+    
+    printBoard(board1);
+    
+    std::string line;
+    while (std::getline(std::cin, line)) {
+        std::cout << "\n>> Move <<\n";
+        std::string input;
+        std::cin >> input;
+        std::vector<std::string> moves = board1.splitString(input, ' ');
+        
+            for(std::string move : moves) {
+                playMove(board1, move);
+                printBoard(board1);
+                std::cout<< "\n>>> Last Move: " << move << "\n";
+            }
+        
+        if (std::cin.eof()) {
+            std::cout << "End of game log..." << std::endl;
+            break;
+        }
+    }
+    return 0;
+}
